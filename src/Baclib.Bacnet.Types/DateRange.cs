849eb954ed247d8a410ef3dfd2ca79b9d7698bb8
The BAClib Initiative <baclib@mailbox.org>
2025-12-22 15:04:12 +0100

    src: add date-range and date-time

---
+++ src/Baclib.Bacnet.Types/DateRange.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: Copyright 2024-2025, The BAClib Initiative and Contributors
+// SPDX-License-Identifier: EPL-2.0
+
+namespace Baclib.Bacnet.Types;
+
+/// <summary>
+/// Represents a BACnetDateRange composed of a start <see cref="Date"/> and an end <see cref="Date"/>.
+/// The range denotes the inclusive interval from <paramref name="StartDate"/> to <paramref name="EndDate"/>.
+/// </summary>
+/// <param name="StartDate">The BACnet <c>Date</c> representing the start of the range. See Clause 20.2.12 for restrictions.</param>
+/// <param name="EndDate">The BACnet <c>Date</c> representing the end of the range. See Clause 20.2.12 for restrictions.</param>
+public readonly record struct DateRange(
+    Date StartDate,
+    Date EndDate)
+{
+}
---
+++ src/Baclib.Bacnet.Types/DateTime.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: Copyright 2024-2025, The BAClib Initiative and Contributors
+// SPDX-License-Identifier: EPL-2.0
+
+namespace Baclib.Bacnet.Types;
+
+/// <summary>
+/// Represents a BACnetDateTime composed of a <see cref="Date"/> and a <see cref="Time"/>.
+/// </summary>
+/// <param name="Date">The BACnet <c>Date</c> component. See Clause 20.2.12 for restrictions.</param>
+/// <param name="Time">The BACnet <c>Time</c> component. See Clause 20.2.13 for restrictions.</param>
+public readonly record struct DateTime(
+    Date Date,
+    Time Time)
+{
+}
