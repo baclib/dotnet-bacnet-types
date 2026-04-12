52aca987b0af093fd10c657b9909ca41e5d2338e
The BAClib Initiative <baclib@mailbox.org>
2026-04-12 09:49:09 +0200

    src: fix numeric primitives

--- src/Baclib.Bacnet.Types/Enumerated8.cs
+++ src/Baclib.Bacnet.Types/Enumerated8.cs
@@ -1,11 +1,4 @@
 // SPDX-FileCopyrightText: Copyright 2024-2026, The BAClib Initiative and Contributors
 // SPDX-License-Identifier: EPL-2.0
 
-namespace Baclib.Bacnet.Types;
-
-/// <summary>
-/// Represents the enumeration Enumerated8 as defined in ANSI/ASHRAE 135-2024 Clause 21.
-/// </summary>
-public enum Enumerated8 : byte
-{
-}
+global using Enumerated8 = byte;
