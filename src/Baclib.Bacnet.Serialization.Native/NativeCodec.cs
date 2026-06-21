e8b51e99dab005c0cac53080ec09e7f9f01f8452
The BAClib Initiative <baclib@mailbox.org>
2026-06-21 10:45:49 +0200

    src: add native codec

---
+++ src/Baclib.Bacnet.Serialization.Native/NativeCodec.cs
@@ -0,0 +1,47 @@
+﻿// SPDX-FileCopyrightText: Copyright 2024-2026, The BAClib Initiative and Contributors
+// SPDX-License-Identifier: EPL-2.0
+
+namespace Baclib.Bacnet.Serialization.Native;
+
+public sealed class NativeCodec<T> where T : ICodec<T>
+{
+    public T Decode(ref NativeReader reader)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Decode(ref NativeReader reader, byte tagNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Optional<T> DecodeOptional(ref NativeReader reader)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Optional<T> DecodeOptional(ref NativeReader reader, byte tagNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Encode(ref NativeWriter writer, in T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Encode(ref NativeWriter writer, byte tagNumber, in T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetSize(in T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetSize(byte tagNumber, in T value)
+    {
+        throw new NotImplementedException();
+    }
+}
